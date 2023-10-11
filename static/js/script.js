4ef72934372d6624742b420c44e19ab38523195d
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-10-11 11:29:32 +0330

    Fix z-index bug

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,19 @@
-// document.getElementById("year").innerHTML = new Date().getFullYear();
+document.getElementById("year").innerHTML = new Date().getFullYear();
 
 // ClassicEditor
 //     .create(document.querySelector('#editor'))
 //     .catch(error => {
 //     console.error(error);
 // });
+
+Dropzone.options.myGreatDropzone = { // camelized version of the `id`
+    paramName: "file", // The name that will be used to transfer the file
+    maxFilesize: 2, // MB
+    dictDefaultMessage: "Drag image here or click to upload",
+    accept: function (file, done) {
+        if (file.name == "justinbieber.jpg") {
+            done("Naha, you don't.");
+        }
+        else { done(); }
+    }
+};
