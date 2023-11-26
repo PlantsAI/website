3850f653a7d3f8080ac309beb204a9b1b1af3cc3
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-11-26 16:05:56 +0330

    Fix onnxruntime bug

--- test.py
+++ test.py
@@ -5,17 +5,12 @@ import onnxruntime
 
 
 parser = argparse.ArgumentParser(description="Get inference from onnx model")
-parser.add_argument("--input", default="static/uploads/image_0241.jpg", type=str)
+parser.add_argument("--input", default="static/uploads/image2.jpg", type=str)
 parser.add_argument("--output", default="./io/output", type=str)
 parser.add_argument("--weights", default="weights/best.onnx", type=str)
-parser.add_argument("--size", default=896, type=int)
-parser.add_argument("--ade20k-info-path", default="./datasets/ade20k_label_colors.txt", type=str)
+parser.add_argument("--size", default=224, type=int)
 parser.add_argument("--thread", default=4, type=int)
-parser.add_argument('--labels', nargs='+', default=[3, 28], help='OneFormer segmentation labels id')
-parser.add_argument('--host', type=str, default='0.0.0.0', help='api host')
-parser.add_argument('--port', type=int, default=8000, help='api port')
 args = parser.parse_args()
-
 
 opts = onnxruntime.SessionOptions()
 opts.intra_op_num_threads = args.thread
@@ -25,10 +20,11 @@ session = onnxruntime.InferenceSession(args.weights, opts, providers=['CUDAExecu
 input_name = session.get_inputs()[0].name
 output_name = session.get_outputs()[0].name
 
-
 image = cv2.imread(args.input)
-image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
-image = image.transpose(2, 0, 1).astype("float32")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image = cv2.resize(image, (args.size, args.size))
+image = image.transpose(2, 0, 1).astype("float32") / 255.0
+image = np.expand_dims(image, axis=0)
 result = session.run([output_name], {input_name: image})
-
+print(np.argmax(result))
 print(result)
